833b35754023acc26b10c96a9a2521d025552065
Aashish Bansal <aashish22bansal@gmail.com>
2023-06-08 00:50:04 +0530

    Angular Project - Search for Products by Category - Code - Define Routes Completed
    Section 12 - Search for Products by Category: 76 Completed
    Angular Project - Search for Products by Category - Code - Defined Routes as a constant array and added them to the imports section in the @NgModule()

--- Source Code/frontend/angular-ecommerce/src/app/app.module.ts
+++ Source Code/frontend/angular-ecommerce/src/app/app.module.ts
@@ -6,12 +6,27 @@ import { ProductListComponent } from './components/product-list/product-list.com
 import { HttpClientModule } from '@angular/common/http';
 import { ProductService } from './services/product.service';
 
+import { Routes, RouterModule } from '@angular/router';
+import { Product } from './common/product';
+
+/**
+ * Defining the Routes.
+ */
+const routes: Routes = [
+  {path: '', redirectTo: '/products', pathMatch: 'full'},
+  {path: 'category', component: ProductListComponent},
+  {path: 'category/:id', component: ProductListComponent},
+  {path: 'products', component: ProductListComponent},
+  {path: '**', redirectTo: '/products', pathMatch: 'full'}
+]
+
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent
   ],
   imports: [
+    RouterModule.forRoot(routes), // Providing the defined routes
     BrowserModule,
     HttpClientModule
   ],
