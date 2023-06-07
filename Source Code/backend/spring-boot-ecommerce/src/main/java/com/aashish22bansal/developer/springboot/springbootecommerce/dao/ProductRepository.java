38ea8c3a27da1bfdae1b2d1f2e3a54ae405866d2
Aashish Bansal <aashish22bansal@gmail.com>
2023-06-08 01:34:58 +0530

    Angular Project - Search for Products by Category - Code - Update Spring Boot Completed
    Section 12 - Search for Products by Category: 79 Completed
    Angular Project - Search for Products by Category - Code - Update Spring Boot Completed
    Added the Query Method to the Spring Boot ProductRepository Interface.

--- Source Code/backend/spring-boot-ecommerce/src/main/java/com/aashish22bansal/developer/springboot/springbootecommerce/dao/ProductRepository.java
+++ Source Code/backend/spring-boot-ecommerce/src/main/java/com/aashish22bansal/developer/springboot/springbootecommerce/dao/ProductRepository.java
@@ -1,9 +1,19 @@
 package com.aashish22bansal.developer.springboot.springbootecommerce.dao;
 
 import com.aashish22bansal.developer.springboot.springbootecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+/**
+ * Adding the Query Method findByCategoryId().
+ * Here, we will match the Category ID using the Parameter Value "id". For this,
+ * Spring Data REST would automatically expose the required endpoint.
+ */
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    // Adding the Query Method
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
