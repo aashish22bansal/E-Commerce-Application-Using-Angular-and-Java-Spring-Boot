67b35fc0d7f09a07a4c25c432ccdbb666c6399b7
Aashish Bansal <aashish22bansal@gmail.com>
2023-06-17 23:22:20 +0530

    Angular Project - Dynamic Search Component - Call Product Service Completed
    Section 13 - Search for Products by Category (Dynamic Search Component): 86 Completed
    Angular Project - Dynamic Search Component - Call Product Service
    In this, we create a new interface GetResponseProductCategory using which we call the REST API which would return an Observable and Map the JSON Data from Spring Data REST to ProductCategory Array and using the interface, we unwrap the JSON from Spring Data REST _embedded entry.

--- Source Code/frontend/angular-ecommerce/src/app/services/product.service.ts
+++ Source Code/frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -3,14 +3,17 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Product } from '../common/product';
 import { map } from 'rxjs/operators';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
+
   // defining the Base URL for the Spring Boot REST API
   private baseUrl = 'http://localhost:8080/api/products';
+  private categoryUrl = 'http://localhost:8080/api/product-category';
 
   constructor(private httpClient: HttpClient) { }
 
@@ -24,16 +27,29 @@ export class ProductService {
     // Building the URL based on Category ID because Spring Data REST would automatically
     // expose the endpoint.
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
+    
 
     // So, we will return the searchUrl instead of the baseUrl
-    return this.httpClient.get<GetResponse>(searchUrl).pipe(
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
+    );
+  }
+
+  getProductCategories(): Observable<ProductCategory[]> {
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+      map(response => response._embedded.productCategory)
     );
   }
 }
 
-interface GetResponse{
+interface GetResponseProducts{
   _embedded: {
     products: Product[];
+  }
+}
+
+interface GetResponseProductCategory{
+  _embedded: {
+    productCategory: ProductCategory[];
   }
 }
