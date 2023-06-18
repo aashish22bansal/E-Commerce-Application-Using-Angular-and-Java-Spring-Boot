37cd7ca5b17227ebc926b0e266fc01c11deffc85
Aashish Bansal <aashish22bansal@gmail.com>
2023-06-18 18:37:10 +0530

    Angular Project - Product Master-Detail View - Enhance the Details Component
    Section 15: Product Master-Detail View - 102 Completed
    Angular Project - Product Master-Detail View - Enhance the Details Component
    Defined Property, injected dependencies for the ProductService and Activated Route, Subscribed to the Params and obtained Product Details

--- Source Code/frontend/angular-ecommerce/src/app/components/product-details/product-details.component.ts
+++ Source Code/frontend/angular-ecommerce/src/app/components/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/common/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-details',
@@ -6,5 +9,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent {
+  // Defining Property for Product
+  product!: Product;
+  
+  // Injecting Dependencies for the ProductService and Activated Route
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute
+  ){}
 
+  // Subscribe on the @params
+  ngOnInit(): void{
+    this.route.paramMap.subscribe(() => {
+      this.handleProductDetails();
+    })
+  }
+  handleProductDetails() {
+    // Obtain the ID Parameter String and convert it to Number using "+" symbol
+    const theProductId: number = +this.route.snapshot.paramMap.get('id')!;
+
+    // With the help of "id", we will use the ProductService
+    this.productService.getProduct(theProductId).subscribe(
+      data => {
+        this.product = data;
+      }
+    );
+  }
 }
